c506670c5e99843333b8af1c05df64e9f0c83cda
Abhijit Zende <abhijitzende75@gmail.com>
2026-01-23 00:41:09 +0530

    add new routes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
 @app.get("/")
 def greet():
     return "Hello from Abhi"
+
+products = [
+    Product(1,"phone","budget phone",99, 10),
+    Product(2,"Laptop","Gaming laptop",999, 5)
+]
+
+@app.get("/products")
+def get_all_products():
+    return products
---
+++ models.py
@@ -0,0 +1,17 @@
+from re import S
+from statistics import quantiles
+
+
+class Product:
+    id: int
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, id:int, name:str, description: str, price: float, quantity: int):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
